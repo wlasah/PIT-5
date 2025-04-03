2a180dc93388f20d0505b35d6fc65fbe48030f37
wlasah <agan.lawrence147@gmail.com>
2025-04-03 13:14:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_SESSION["user_id"])) {
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h2>Welcome to phpACT</h2>
+    <h2>Welcome to PHP-MySQL Activity</h2>
     <p>Please choose an option:</p>
     <a href="login.php">Login</a> | <a href="register.php">Register</a>
 </body>
